416b7a6f47b49d4ed92aa852499f2439d8b3c217
manal shehri <manal@intern.tera-cit.net>
2020-12-23 15:44:52 +0300

    Done Search Artist

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ def search_venues():
     dataDict['id'] = venue.id 
     dataDict['name'] = venue.name
     response['data'].append(dataDict.copy())
-    
+
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
@@ -376,14 +376,20 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
+  
+  search =request.form.get('search_term', '')
+  artists = Artist.query.filter(Artist.name.ilike('%'+search+'%')).all()
+  # source for how to deal with ilike: https://docs.sqlalchemy.org/en/14/orm/internals.html?highlight=ilike#sqlalchemy.orm.attributes.QueryableAttribute.ilike
+
+  response = {}
+  dataDict = {}
+  response['count'] = len(artists)
+  response['data'] = []
+
+  for artist in artists:
+    dataDict['id'] = artist.id 
+    dataDict['name'] = artist.name
+    response['data'].append(dataDict.copy())
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
